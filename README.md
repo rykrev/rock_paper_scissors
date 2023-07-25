798620ae28ea1fa9233b10615b32c130eef16f91
rykrev <hugo773344@gmail.com>
2023-07-24 22:28:31 -0400

    Wrote out boilerplate and instructions in README file

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # rock_paper_scissors
 JavaScript RPS Game.
+
+This game will simulate a game of Rock Paper Scissors against a computer. 
+After each round, the console will display who won and what move beats the opponent's (computer) move.
+After five rounds, the console will return who won the majority of those rounds. 
+Have fun!
---
+++ javascript.js
@@ -0,0 +1,8 @@
+// This game will simulate a game of Rock Paper Scissors against a computer. 
+// The player will input their choice into a prompt through a playerChoice variable.
+// The variable will
+// The computer will randomly either return "Rock", "Paper", or "Scissors" through a getComputerChoice function in the console each round.
+// The result of the getComputerChoice function will equal the computerSelection paramter in the playRound function.
+// The playRound function will determine who won the round.
+// After each round, return a string through that function that says who won and what move beats the opponent's (computer) move.
+// After five rounds, the console will return who won the majority of those in a game() function.
